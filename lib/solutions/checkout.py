9ed553df0d73e845530caf94de4c9eda8ac8d983
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-08 15:57:16 +0000

    Fri Dec 08 15:57:16 GMT 2017

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,22 +1,36 @@
 def criteria(consume, basket):
     valid = True
     for k in consume:
-        if not (k in basket and basket[k] >= consume[k]):
+        if len(k) > 1:
+            amt = 0
+            for i in k:
+                amt += basket[i]
+                if amt >= consume[k]:
+                    return True
+        elif not (k in basket and basket[k] >= consume[k]):
             valid = False
     return valid
 
 def consume(consume, basket):
     for k in consume:
-        basket[k] -= consume[k]
+        if len(k) > 1:
+            for i in k:
+                amt = 0
+                if (amt + basket[i]) > consume[k]:
+                    basket[i] -= consume[k] - amt
+                else:
+                    amt += basket[i]
+        else:
+            basket[k] -= consume[k]
 
 # noinspection PyUnusedLocal
 def checkout(skus):
     prices = {
         'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40, 'F': 10,
-        'G': 20, 'H': 10, 'I': 35, 'J': 60, 'K': 80, 'L': 90,
+        'G': 20, 'H': 10, 'I': 35, 'J': 60, 'K': 70, 'L': 90,
         'M': 15, 'N': 40, 'O': 10, 'P': 50, 'Q': 30, 'R': 50,
-        'S': 30, 'T': 20, 'U': 40, 'V': 50, 'W': 20, 'X': 90,
-        'Y': 10, 'Z': 50
+        'S': 20, 'T': 20, 'U': 40, 'V': 50, 'W': 20, 'X': 17,
+        'Y': 20, 'Z': 21
     }
     deals = [
         {'consume': {'A': 5}, 'output': 200, 'savings': 50},
@@ -29,6 +43,7 @@ def checkout(skus):
         {'consume': {'A': 3}, 'output': 130, 'savings': 20},
         {'consume': {'B': 2}, 'output': 45, 'savings': 15},
         {'consume': {'N': 3, 'M': 1}, 'output': 120, 'savings': 15},
+        {'consume': {'STXYZ': 3}, 'output': 45, 'savings': 15}
         {'consume': {'K': 2}, 'output': 150, 'savings': 10},
         {'consume': {'V': 2}, 'output': 90, 'savings': 10},
         {'consume': {'Q': 3}, 'output': 80, 'savings': 10},
