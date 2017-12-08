e0b3ebb9aa74f6f86304d2195e9f222326330f11
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-08 11:15:20 +0000

    Fri Dec 08 11:15:20 GMT 2017

--- lib/solutions/sum.py
+++ lib/solutions/sum.py
@@ -1,8 +1,8 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def sum(x, y):
-    if not 0 < x < 100:
+    if not 0 <= x <= 100:
         raise ValueError('arg x must be between 0 and 100')
-    if not 0 < x < 100:
+    if not 0 <= x <= 100:
         raise ValueError('arg y must be between 0 and 100')
 
     return x + y
