6fb4522a8bc87d0450d8ece339a3a8a92ed35d8d
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-08 12:20:35 +0000

    Fri Dec 08 12:20:35 GMT 2017

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,7 +1,11 @@
 # noinspection PyUnusedLocal
 def checkout(skus):
     prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
-    deals = {'A': (3, 130), 'B': (2, 45)}
+    deals = {
+        'A': [(5, 200), (3, 130)],
+        'B': [(2, 45)],
+        'E': [(2, {'B': 1})]
+    }
     basket = {}
 
     # if not isinstance(skus, str):
@@ -17,10 +21,20 @@ def checkout(skus):
 
     total = 0
     for item in basket:
-        if item in deals and basket[item] >= deals[item][0]:
+        if item in deals:
+            amount = basket[item]
+            for deal in deals[item]:
+                while amount > deal[0]:
+                    if isinstance(deal[1], int):
+                        total += deal[1]
+                    elif isinstance(deal[1], dict):
+                        # .. what if B is part of another deal... which takes precedence?
+                        for k in deal[1]:
+                            if k in basket:
+                                basket[k] -= deal[1][k]
+                    amount -= deal[0]
+            total += amount * prices[item]
             # excess
-            total += (basket[item] // deals[item][0]) * deals[item][1]
-            total += (basket[item] % deals[item][0]) * prices[item]
         else:
             total += (basket[item] * prices[item])
 
