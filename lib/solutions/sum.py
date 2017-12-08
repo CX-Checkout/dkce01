2e7de0d216a7a8a636f8fb8f7f323fa10ba4e9f8
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-08 11:13:07 +0000

    Fri Dec 08 11:13:07 GMT 2017

--- lib/solutions/sum.py
+++ lib/solutions/sum.py
@@ -1,3 +1,8 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def sum(x, y):
-    raise NotImplementedError()
+    if not 0 < x < 100:
+        raise ValueError('arg x must be between 0 and 100')
+    if not 0 < x < 100:
+        raise ValueError('arg y must be between 0 and 100')
+
+    return x + y
