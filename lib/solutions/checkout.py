e7611ec810a17d7d125a3a2cded328224203cba1
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-08 14:17:23 +0000

    Fri Dec 08 14:17:23 GMT 2017

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,23 +1,11 @@
-def criteria(consume, basket):
-    valid = True
-    for k in consume:
-        if not (k in basket and basket[k] >= consume[k]):
-            valid = False
-    return valid
-
-def consume(consume, basket):
-    for k in consume:
-        basket[k] -= consume[k]
-
 # noinspection PyUnusedLocal
 def checkout(skus):
     prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40}
-    deals = [
-        {'consume': {'A': 5}, 'output': 200, 'savings': 50},
-        {'consume': {'E': 2, 'B': 1}, 'output': 80, 'savings': 30},
-        {'consume': {'A': 3}, 'output': 130, 'savings': 20},
-        {'consume': {'B': 2}, 'output': 45, 'savings': 15},
-    ]
+    deals = {
+        'A': [(5, 200), (3, 130)],
+        'B': [(2, 45)],
+        'E': [(2, {'B': 1})]
+    }
     basket = {}
 
     # if not isinstance(skus, str):
@@ -32,15 +20,22 @@ def checkout(skus):
             basket[item] = 1
 
     total = 0
-
-    ##greedy algorithm
-    tmp_basket = basket.copy()
-    for deal in deals:
-        while criteria(deal['consume'], tmp_basket):
-            consume(deal['consume'], tmp_basket)
-            total += deal['output']
-
-    for item in tmp_basket:
-        total += tmp_basket[item] * prices[item]
+    for item in basket:
+        if item in deals:
+            amount = basket[item]
+            for deal in deals[item]:
+                print(deal)
+                while amount >= deal[0]:
+                    if isinstance(deal[1], int):
+                        total += deal[1]
+                    elif isinstance(deal[1], dict):
+                        for k in deal[1]:
+                            if k in basket and basket[k] >= deal[1][k]:
+                                total -= deal[1][k] * prices[k]
+                        total += deal[0] * prices[item]
+                    amount -= deal[0]
+            total += amount * prices[item]
+        else:
+            total += (basket[item] * prices[item])
 
     return total
