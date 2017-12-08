999c68e29e266500b75108f63e099c7c1dc95144
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-08 11:24:30 +0000

    Fri Dec 08 11:24:30 GMT 2017

--- lib/solutions/hello.py
+++ lib/solutions/hello.py
@@ -1,3 +1,5 @@
 # noinspection PyUnusedLocal
 def hello(friend_name):
-    raise NotImplementedError()
+    if not isinstance(friend_name, str):
+        raise ValueError('arg must be of type string')
+    return 'Hello World!'
