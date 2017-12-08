daf631a4d4a405467faa18d1257f9230ec32522d
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-08 11:48:32 +0000

    Fri Dec 08 11:48:32 GMT 2017

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,3 +1,25 @@
 # noinspection PyUnusedLocal
 def checkout(skus):
-    raise NotImplementedError()
+    prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+    deals = {'A': (3, 130), 'B': (2, 45)}
+    basket = {}
+
+    if not isinstance(skus, str):
+        raise ValueError('skus must be str')
+
+    for item in skus:
+        if item not in prices:
+            return -1
+        if item in basket:
+            basket[item] += 1
+        else:
+            basket[item] = 1
+
+    total = 0
+    for item in basket:
+        total += (basket[item] % deals[item][0]) * prices[item]
+        if basket[item] >= deals[item][0]:
+            # excess
+            total += (basket[item] // deals[item][0]) * deals[item][1]
+
+    return total
