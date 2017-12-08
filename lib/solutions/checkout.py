87a9e75ee610ff7643e87f0513843af41892fcb3
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-08 14:26:23 +0000

    Fri Dec 08 14:26:23 GMT 2017

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,11 +1,12 @@
 # noinspection PyUnusedLocal
 def checkout(skus):
     prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40}
-    deals = {
-        'A': [(5, 200), (3, 130)],
-        'B': [(2, 45)],
-        'E': [(2, {'B': 1})]
-    }
+    deals = [
+        {'consume': {'A': 5}, 'output': 200, 'savings': 50},
+        {'consume': {'E': 2, 'B': 1}, 'output': 80, 'savings': 30},
+        {'consume': {'A': 3}, 'output': 130, 'savings': 20},
+        {'consume': 'BB', 'output': 45, 'savings': 15},
+    ]
     basket = {}
 
     # if not isinstance(skus, str):
@@ -20,6 +21,9 @@ def checkout(skus):
             basket[item] = 1
 
     total = 0
+    for item in basket:
+        total += basket[item] * prices[item]
+
     for item in basket:
         if item in deals:
             amount = basket[item]
