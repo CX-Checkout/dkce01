14adca63799cf3331ce2d9f209d30581b672c69e
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-08 15:14:23 +0000

    Fri Dec 08 15:14:23 GMT 2017

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -37,8 +37,10 @@ def checkout(skus):
     for deal in deals:
         while criteria(deal['consume'], tmp_basket):
             total += deal['output']
+            print(total)
 
     for item in tmp_basket:
         total += tmp_basket[item] * prices[item]
+        print(total)
 
     return total
